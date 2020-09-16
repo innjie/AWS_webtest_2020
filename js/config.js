b8f0bb8d124feb9b41f284c67c9c9286f714ac8c
김유진 <yujindd@naver.com>
2020-09-16 23:38:13 +0900

    clientid 새로운 클라이언트로변경

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_alLPoMsaE', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-2', // e.g. us-east-2
-		clientId: 'jd2e1po4ddtq5u9l3020kr1u0' //is this used anywhere?
+		clientId: '7757kcu7e4ch62dujts26qjg2s' //is this used anywhere?
     },
 };
 
